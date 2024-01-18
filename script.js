75f050c5ae322895abf023eed23b9cb1d70b5103
lexiconlink <155447337+lexiconlink@users.noreply.github.com>
2024-01-19 03:43:38 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+
+        if ($target.length) {
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 900, 'swing', function() {
+                window.location.hash = target;
+            });
+        }
+    });
+});
